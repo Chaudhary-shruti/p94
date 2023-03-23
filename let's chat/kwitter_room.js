1d34172c64c1b177beb6cf6eef6d17e0e62a92a9
Chaudhary-shruti <93135302+Chaudhary-shruti@users.noreply.github.com>
2023-03-23 00:35:07 -0700

    Add files via upload

---
+++ let's chat/kwitter_room.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDyzQJFqpdvmUbaBa7LWKYOnYKezA0F9No",
+    authDomain: "project-94-1aaaf.firebaseapp.com",
+    projectId: "project-94-1aaaf",
+    storageBucket: "project-94-1aaaf.appspot.com",
+    messagingSenderId: "766101281489",
+    appId: "1:766101281489:web:5fbff7800517d404bfc3f0"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+
+logout(){
+    window.location="index.html"
+}
